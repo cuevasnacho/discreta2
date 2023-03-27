0e2403c7c15cdf342bff74048c386b59c546e1d9
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-03-27 15:31:00 -0300

    probando funciones

--- APIG23.c
+++ APIG23.c
@@ -1,21 +1,24 @@
-
+#include <stdlib.h>
+#include "APIG23.h"
+#include "vector.h"
 
 Grafo ConstruirGrafo() {
     Grafo g = (Grafo)malloc(sizeof(GrafoSt));
-    
     char c;
 
     while ((c = getchar()) != EOF) {
         if (c == 'c') {
             while (c != '\n')
                 c = getchar();
-        } else if (c == 'p') {
+        }
+        else if (c == 'p') {
             char word[5];
             scanf("%4s %d %d\n",word,&g->V,&g->E);
             g->init = (bool*)calloc(sizeof(g->V), sizeof(bool));
             g->vertex = (vector*)calloc(g->V, sizeof(vector));
             break;
-        } else 
+        }
+        else 
             return NULL;
     }
     for(u32 i=0; i<g->E; i++) {
@@ -33,5 +36,14 @@ Grafo ConstruirGrafo() {
         vector_pushback(g->vertex[x], y);
         vector_pushback(g->vertex[y], x);
     }
+
+    for(u32 i=0; i<g->V; ++i) {
+        printf("%u ", i);
+        for(u32 j=0; j<vector_size(g->vertex[i]); ++j) {
+            printf("%u ",vector_i(g->vertex[i],j));
+        }
+        printf("\n");
+    }
+
     return g;
 }
--- EstructuraGrafo23.h
+++ EstructuraGrafo23.h
@@ -1,9 +1,10 @@
 #ifndef ESTRUC_GRAF_H
 #define ESTRUC_GRAF_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include "vector.h"
+
+typedef unsigned int u32;
 
 typedef struct GrafoSt {
     vector* vertex;
@@ -12,27 +13,5 @@ typedef struct GrafoSt {
     u32 E;
     u32 degree;
 } GrafoSt;
-
-/**********CONSTRUCTORES***************/
-
-Grafo ConstruirGrafo(void);
-
-void DestruirGrafo(Grafo G);
-
-/**********FUNCIONES GRAFO*************/
-
-u32 NumeroDeVertices(Grafo G);
-
-u32 NumeroDeLados(Grafo G);
-
-u32 Delta(Grafo G);
-
-/*******FUNCIONES VERTICES*************/
-
-u32 Nombre(u32 i,Grafo G);
-
-u32 Grado(u32 i,Grafo G);
-
-u32 IndiceVecino(u32 j,u32 i,Grafo G);
 
 #endif
