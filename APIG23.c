9d6dbbcecca36018a4ca041b6955033906f98b85
Tomas's Home <tomasmarmay21@gmail.com>
2023-03-30 22:02:26 -0300

    lee los datos bien

--- APIG23.c
+++ APIG23.c
@@ -43,6 +43,7 @@ Grafo ConstruirGrafo() {
     }
     //abrir el archivo leerlo hacer binary y encontrar el primo siguiente a g-V
     u32 ht_size,i,hash,x,y;
+    ht_size = 5;
     u32* hash_table = (u32*)calloc(ht_size,sizeof(u32));
     
 
@@ -57,43 +58,45 @@ Grafo ConstruirGrafo() {
         vector_pushback(con2,y);
         
         hash = hash_func(x,ht_size);
-        
         if (!hash_table[hash])
             hash_table[hash] = x;
-        
         else if (hash_table[hash] == x)
             continue;
-
         else
             vector_pushback(v,x);
         
         hash = hash_func(y,ht_size);
         if (!hash_table[hash])
             hash_table[hash] = y;
-
         else if (hash_table[hash] == y)
             continue;
-
         else
             vector_pushback(v,y);
     }
+    
+    /* Muestra conecciones */ 
+    for (i = 0; i < g->E; i++){
+        printf("%d ", hash_table[i]);
+    }
+    printf("\n");
 
+    /* Encontrar un lugar para las coliciones */
     for (i = 0; i < vector_size(v); i++){
         u32 vi = vector_i(v,i);
         hash = hash_func(vi+1,ht_size);
-        while(!hash_table[hash] && hash_table[hash] != hash)
-            i = hash_func(i+1,ht_size);
+        while(hash_table[hash] && hash_table[hash] != vi)
+            hash = hash_func(hash+1,ht_size);
 
         if (!hash_table[hash])
             hash_table[hash] = vi;
-
     }
-    u32 fixing = 0;
     u32 j;
     i = 0;
     u32* fix_index = (u32*)calloc(ht_size,sizeof(u32));
+    u32 fixing = 0;
 
-    while(i < vector_size(v)){
+    /* Acomoda los indices */
+    while(i < ht_size && fixing < ht_size){
         if (!hash_table[i]){
             j = i;
             while(!hash_table[j]){
@@ -103,26 +106,52 @@ Grafo ConstruirGrafo() {
             i = (j+1) % ht_size;
         }
         else{
-            fix_index[fixing] = i ;
+            fix_index[fixing] = i;
             i++;
         }       
         fixing++;
     }
-
-
     //ahora si quiero obtener el nodo i en el orden natural tengo que hacer
     //hash_table[fix_index[i]]
 
-    //rearmar conexiones
-    
+    /* Muestra conecciones */ 
+    for (i = 0; i < g->E; i++){
+        printf("%d ", fix_index[i]);
+    }
+    printf("\n");
+
+    /* armar conexiones con el nuevo mapeo */
     for (i = 0; i < g->E; i++){
         x = hash_func(vector_i(con1,i),ht_size);
         y = hash_func(vector_i(con2,i),ht_size);
+        
+        //lo tiene que buscar                 
+        while(hash_table[x] != vector_i(con1,i))
+            x = hash_func(x+1,ht_size);         
+        
+        while(hash_table[y] != vector_i(con2,i))
+            y = hash_func(y+1,ht_size);
+        
+        if (!g->vertex[x])
+            g->vertex[x] = vector_init();
+        if (!g->vertex[y])
+            g->vertex[y] = vector_init();
+                  
 
         vector_pushback(g->vertex[x],y);
         vector_pushback(g->vertex[y],x);
-        
     }
+        /* Muestra conecciones */ 
+    for (i = 0; i <= g->V; i++){
+        if (g->vertex[i]){
+            printf("%d :",i);
+            for (j = 0; j < vector_size(g->vertex[i]); j++){        
+                printf("%d ",vector_i(g->vertex[i],j));
+            }
+            printf("\n");
+        }
+    }
+    //falta hacer los frees
 
     return g;
 }
