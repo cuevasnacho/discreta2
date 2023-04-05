f3c345d5e84451c8e89975f7b5c38a4a36f0cb38
Tomas's Home <tomasmarmay21@gmail.com>
2023-04-05 12:05:14 -0300

    solucionando problema caso borde

--- test/main.c
+++ test/main.c
@@ -1,44 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-char buf[100]={0};
-char number[100]={0};
-
-int ignore_blank(int j) {
-    while(buf[j]==' ') ++j;
-    return j;
-}
-
-int get_number(int j) {
-    int v, n=0;
-    while(buf[j]!=' ' && buf[j]!='\0') {
-        number[n] = buf[j];
-        ++n; ++j;
-    }
-    number[n]='\0';
-    v = atoi(number);
-    return v;
-}
+#include "APIG23.h"
 
 int main() {
-    int e, v;
-    buf[0]='p';
-    for(int i=0; i<5; ++i) {
-        fgets(buf,"%s",stdin);
-        if (buf[0]=='p'){
-            int j=7;
-            v=get_number(j);
-            j=ignore_blank(j);
-            e=get_number(j);
-            printf("\n%d %d",v,e);
-        } else if (buf[0]=='e'){
-            int j=2;
-            v=get_number(j);
-            j=ignore_blank(j);
-            e=get_number(j);
-            printf("\n%d %d",v,e);
-        } else continue;
-    }
-
+    Grafo g = ConstruirGrafo();
+    //printf("delta %u\n",Delta(g));
+    //printf("numero de lados %u\n",NumeroDeLados(g));
+    //printf("numero de vertices %u\n",NumeroDeVertices(g));
+    //for (int i = 0; i < 6; i++){
+    //    printf("Lado %u, grado %u \n",Nombre(i,g),Grado(i,g));
+    //}
+    DestruirGrafo(g);
     return 0;
 }
