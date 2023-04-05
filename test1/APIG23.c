93e0f808a89ddca16d10f8ff8be2761dfbceb238
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-04-05 10:45:41 -0300

    formato y robustez del programa

--- test1/APIG23.c
+++ test1/APIG23.c
@@ -3,15 +3,15 @@
 #include "APIG23.h"
 #include "vector.h"
 
-
 inline static u32 hash_func(u32 a, u32 size){
     return a%size;
 }
 
-/* Constructores */
-static u32 max(u32 a, u32 b) {
+inline static u32 max(u32 a, u32 b) {
     return (a > b) ? a : b;   
 }
+
+/* Constructores */
 Grafo ConstruirGrafo() {
     u32 v_size,i,hash,x,y;
     char c;
@@ -25,7 +25,8 @@ Grafo ConstruirGrafo() {
         }
         else if (c == 'p') {
             char word[5];
-            scanf("%4s %d %d\n",word,&g->V,&g->E);
+            if (!scanf("%4s %d %d\n",word,&g->V,&g->E))
+                printf("Error leyendo los valores");
             g->vertex = (vector*)calloc(g->V, sizeof(vector));
             g->name = (u32*)calloc(g->V, sizeof(u32));
             g->init = (bool*)calloc(g->V, sizeof(bool));
@@ -43,8 +44,10 @@ Grafo ConstruirGrafo() {
     vector con2 = vector_init(g->E);
     vector v = vector_init(v_size);
 
+    /* Pedir los lados y ubicar los primeros vertices */
     for(i=0; i<g->E; i++) {
-        scanf("%c %d %d\n",&c,&x,&y);
+        if (!scanf("%c %d %d\n",&c,&x,&y))
+            printf("Error leyendo los valores");
         
         vector_pushback(con1,x);
         vector_pushback(con2,y);
@@ -61,9 +64,8 @@ Grafo ConstruirGrafo() {
         else if (g->name[hash] != y)
             vector_pushback(v,y);
     }
-    
+
     /* Encontrar un lugar para las coliciones */
-    //printf("Cantidad de coliciones: %d\n",vector_size(v));
     for (i = 0; i < vector_size(v); i++){
         u32 vi = vector_i(v,i);
         hash = hash_func(vi+1,v_size);
@@ -71,12 +73,10 @@ Grafo ConstruirGrafo() {
             hash = hash_func(hash+1,v_size);
         g->name[hash] = vi;
     }
-    
     vector_destroy(v);
 
-    /* armar conexiones con el nuevo mapeo */
+    /* Armar conexiones con el nuevo mapeo */
     double coneccion_qty = ceil((0.0001*g->E)/100);
-    //printf("Reaarmo conexiones \n");
     for (i = 0; i < g->E; i++) {
         x = hash_func(vector_i(con1,i),v_size);
         while(g->name[x] != vector_i(con1,i))
@@ -101,15 +101,14 @@ Grafo ConstruirGrafo() {
         g->name[x] = vector_i(con1,i);
         g->name[y] = vector_i(con2,i);
     }
+    vector_destroy(con1);
+    vector_destroy(con2);
 
+    /* Calcular el delta del grafo */
     for (i=0; i<g->V; ++i) {
         if(g->init[i])
             g->degree = max(g->degree,vector_size(g->vertex[i]));
     }
-
-    //printf("Destruyo \n");
-    vector_destroy(con1);
-    vector_destroy(con2);
 
     return g;
 }
@@ -129,30 +128,33 @@ void DestruirGrafo(Grafo G) {
     G = NULL;
 }
 
-/* Grafo info */
-
+/* Informacion del Grafo */
 u32 NumeroDeVertices(Grafo G) {
     return G->V;
 }
+
 u32 NumeroDeLados(Grafo G) {
     return G->E;
 }
+
 u32 Delta(Grafo G){
     return G->degree;
 }
 
-/* Vertices info*/
+/* Informacion de los Vertices */
 
 u32 Nombre(u32 i,Grafo G){
     return G->name[i];
 }
+
 u32 Grado(u32 i,Grafo G){
     if(G->init[i])
         return vector_size(G->vertex[i]);
     return 0;
 }
+
 u32 IndiceVecino(u32 j,u32 i,Grafo G){
-    if (i < MAX_RANGE && j < vector_size(G->vertex[i]))
+    if (i < G->V && j < vector_size(G->vertex[i]))
         return vector_i(G->vertex[i],j);
     else
         return MAX_RANGE;
