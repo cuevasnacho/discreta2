a88de7bacb2e360f48b236d55cf83e80ec0f92ac
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-04-07 16:52:46 -0300

    formato en APIG23.

--- test/APIG23.h
+++ test/APIG23.h
@@ -1,41 +1,31 @@
 #ifndef APIG23_H
 #define APIG23_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <math.h>
 
-#define MAX_RANGE UINT_MAX  
-
-//el .h de abajo debe tener definida GrafoSt y u32.
 #include "EstructuraGrafo23.h"
 
-
+#define MAX_RANGE UINT_MAX  
 
 //Grafo es un puntero a una estructura GrafoSt, la cual debe estar definida en el .h de arriba
-
 typedef GrafoSt *Grafo;
 
-
-//construcci'on/destrucci'on
-
-//debe leer desde stdin
+// Constructores
 Grafo ConstruirGrafo();
-
 void DestruirGrafo(Grafo G);
 
 
-
-//funciones para extraer datos del grafo. u32 debe estar definida en el .h de arriba
+// funciones para extraer datos del grafo. u32 debe estar definida en el .h de arriba
 
 u32 NumeroDeVertices(Grafo G);
 u32 NumeroDeLados(Grafo G);
 u32 Delta(Grafo G);
 
 
-//funciones de extraccion de informacion de vertices 
+// funciones de extraccion de informacion de vertices 
 
 u32 Nombre(u32 i,Grafo G);
 u32 Grado(u32 i,Grafo G);
