9e8e3d1d206f3a0945f02c10dd90c5aff1e5a479
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-04-07 15:51:03 -0300

    main

--- test/main.c
+++ test/main.c
@@ -8,9 +8,11 @@ int main() {
     printf("numero de lados %u\n",NumeroDeLados(g));
     printf("numero de vertices %u\n",NumeroDeVertices(g));
 
+    /*
     for (u32 i = 0; i < g->V; i++){
         printf("Lado %u, grado %u \n",Nombre(i,g),Grado(i,g));
     }
+    */
     
     DestruirGrafo(g);
     return 0;
