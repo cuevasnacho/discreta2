cf3db03ff1bc4ba66b88f41f718c2aa50b795b74
Tomas's Home <tomasmarmay21@gmail.com>
2023-04-18 14:37:42 -0300

    quickstart

---
+++ greedy/APIParte2.c
@@ -0,0 +1,9 @@
+#include <APIParte2.h>
+
+/*
+u32 Greedy(Grafo G,u32* Orden,u32* Color);
+
+char OrdenImparPar(u32 n,u32* Orden,u32* Color);
+
+char OrdenJedi(Grafo G,u32* Orden,u32* Color);
+*/
---
+++ greedy/APIParte2.h
@@ -0,0 +1,7 @@
+#include <../construccion-grafo/APIG23.h>
+
+u32 Greedy(Grafo G,u32* Orden,u32* Color);
+
+char OrdenImparPar(u32 n,u32* Orden,u32* Color);
+
+char OrdenJedi(Grafo G,u32* Orden,u32* Color);
