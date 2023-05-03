3723034729ae47082242334bd59bfb96c3520d1c
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-05-02 23:21:42 -0300

    tests basicos pasados

--- greedy/APIParte2.c
+++ greedy/APIParte2.c
@@ -1,8 +1,8 @@
+#include <stdbool.h>
+
 #include "APIParte2.h"
 
-typedef unsigned int u32;
-
-inline static u32 max(u32 x, u32 y) {
+inline static u32 maxx(u32 x, u32 y) {
     return (x>y)?x:y;
 }
 
@@ -10,17 +10,16 @@ u32 Greedy(Grafo G,u32* Orden,u32* Color) {
     u32 V = NumeroDeVertices(G);
 
     /* Propiedad de grafo completo */
-    if ((V*(V-1))/2 == NumeroDeLados(G)){
-        return V;
-    }
+    //if ((V*(V-1))/2 == NumeroDeLados(G)){
+    //    return V;
+    //}
 
     u32 max_color = 1;
     bool* coloured = calloc(V, sizeof(bool));   // dice si el i-esimo nodo esta coloreado
     bool* color_set = calloc(V, sizeof(bool));    // dice si el i-esimo color esta usado
 
     if (coloured == NULL || color_set == NULL){
-        printf("Error en greedy \n");
-        return MAX_RANGE;
+        return (u32)(-1);
     }
 
     Color[Orden[0]] = 0;
@@ -32,13 +31,13 @@ u32 Greedy(Grafo G,u32* Orden,u32* Color) {
             for (u32 j = 0; j < Grado(Orden[i],G); j++) {
                 if (coloured[IndiceVecino(j,Orden[i],G)]) {
                     color_set[Color[IndiceVecino(j,Orden[i],G)]] = true;
-                    max_vec = max(max_vec, Color[IndiceVecino(j,Orden[i],G)]);
+                    max_vec = maxx(max_vec, Color[IndiceVecino(j,Orden[i],G)]);
                 }
             }
             while (color_set[min_number]) {
                 min_number++;
             }
-            max_color = max(max_color, min_number);
+            max_color = maxx(max_color, min_number);
         }
         Color[Orden[i]] = min_number;
         coloured[Orden[i]] = true;
@@ -52,7 +51,7 @@ u32 Greedy(Grafo G,u32* Orden,u32* Color) {
     free(color_set);
     color_set = NULL;
 
-    return max_color+1;
+    return (u32)(max_color+1);
 }
 
 char OrdenImparPar(u32 n,u32* Orden,u32* Color) {
@@ -61,8 +60,15 @@ char OrdenImparPar(u32 n,u32* Orden,u32* Color) {
         u32 *x = (u32 *)a;
         u32 *y = (u32 *)b;
 
-        if (Color[*x]%2 == Color[*y]%2)
-            return (Color[*y] - Color[*x]);
+        if (Color[*x]%2 == Color[*y]%2) {
+            if (Color[*y] > Color[*x]) {
+                return 1;
+            } else if (Color[*y] < Color[*x]) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
         else
             if (Color[*x]%2)
                 return -1;
@@ -80,14 +86,13 @@ char OrdenJedi(Grafo G,u32* Orden,u32* Color) {
     u32* sums = calloc(NumeroDeVertices(G), sizeof(u32));
     
     if (sums == NULL){
-        printf("Error en orden jedi \n");
         return (char)1;
     }
     // calculo F(x) para cada color
-    for (u32 i = 1; i < NumeroDeVertices(G); ++i) {
+    for (u32 i = 0; i < NumeroDeVertices(G); ++i) {
         sums[Color[i]] += Grado(i,G);
     }
-    for (u32 i = 1; i < NumeroDeVertices(G); ++i) {
+    for (u32 i = 0; i < NumeroDeVertices(G); ++i) {
         if (sums[i]) sums[i] *= i;
         else break;
     }
@@ -96,7 +101,13 @@ char OrdenJedi(Grafo G,u32* Orden,u32* Color) {
         u32 *x = (u32 *)a;
         u32 *y = (u32 *)b;
         
-        return (sums[Color[*y]] - sums[Color[*x]]);
+        if (sums[Color[*y]] > sums[Color[*x]]) {
+            return 1;
+        } else if (sums[Color[*y]] < sums[Color[*x]]) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
     // ordeno los vertices con la funcion custom
